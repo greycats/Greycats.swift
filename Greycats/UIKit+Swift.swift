98ab908c39bae564795059e4922110390d462c97
Rex Sheng <shengning@gmail.com>
2015-06-02 14:39:56 -0700

    Update UIKit+Swift.swift

--- Greycats/UIKit+Swift.swift
+++ Greycats/UIKit+Swift.swift
@@ -1,9 +1,12 @@
 //
 //  UIKit+Swift.swift
+//  ThinkSpider
 //
 //  Created by Rex Sheng on 3/11/15.
 //  Copyright (c) 2015 Rex Sheng. All rights reserved.
 //
+
+// available in pod 'Greycats', '~> 0.2.0'
 
 import UIKit
 let iOS8Less = (UIDevice.currentDevice().systemVersion as NSString).floatValue < 8
@@ -34,7 +37,7 @@ extension UIColor {
 	}
 }
 
-private var bitmapInfo: CGBitmapInfo = {
+var bitmapInfo: CGBitmapInfo = {
 	var bitmapInfo = CGBitmapInfo.ByteOrder32Little
 	bitmapInfo &= ~CGBitmapInfo.AlphaInfoMask
 	bitmapInfo |= CGBitmapInfo(CGImageAlphaInfo.PremultipliedFirst.rawValue)
@@ -42,21 +45,22 @@ private var bitmapInfo: CGBitmapInfo = {
 	}()
 
 extension CGImage {
-	func blend(mode: CGBlendMode, color: CGColor) -> CGImage {
+	func blend(mode: CGBlendMode, color: CGColor, alpha: CGFloat = 1) -> CGImage {
 		let colourSpace = CGColorSpaceCreateDeviceRGB()
 		let width = CGImageGetWidth(self)
 		let height = CGImageGetHeight(self)
 		let rect = CGRect(x: 0, y: 0, width: CGFloat(width), height: CGFloat(height))
-		let context = CGBitmapContextCreate(nil, width, height, CGImageGetBitsPerComponent(self), width * 4, colourSpace, CGImageGetBitmapInfo(self))
+		let context = CGBitmapContextCreate(nil, width, height, CGImageGetBitsPerComponent(self), width * 4, colourSpace, bitmapInfo)
 		
 		CGContextSetFillColorWithColor(context, color)
 		CGContextFillRect(context, rect)
 		CGContextSetBlendMode(context, mode)
+		CGContextSetAlpha(context, alpha)
 		CGContextDrawImage(context, rect, self)
 		return CGBitmapContextCreateImage(context)
 	}
 	
-	class func op(width: UInt, _ height: UInt, closure: (CGContextRef) -> Void) -> CGImage! {
+	class func op(width: Int, _ height: Int, closure: (CGContextRef) -> Void) -> CGImage! {
 		let colourSpace = CGColorSpaceCreateDeviceRGB()
 		let rect = CGRect(x: 0, y: 0, width: CGFloat(width), height: CGFloat(height))
 		let context = CGBitmapContextCreate(nil, width, height, 8, width * 4, colourSpace, bitmapInfo)
@@ -65,7 +69,7 @@ extension CGImage {
 	}
 	
 	class func create(color: CGColor, size: CGSize) -> CGImage! {
-		return op(UInt(size.width), UInt(size.height)) { (context) in
+		return op(Int(size.width), Int(size.height)) { (context) in
 			let rect = CGRect(origin: .zeroPoint, size: size)
 			CGContextSetFillColorWithColor(context, color)
 			CGContextFillRect(context, rect)
@@ -99,11 +103,11 @@ extension UIView {
 	}
 }
 
-extension UIView {
+extension NSObject {
 	func loadFromNib(nibName: String, index: Int = 0) -> UIView {
 		let buddle = NSBundle(forClass: self.dynamicType)
 		let nib = UINib(nibName: nibName, bundle: buddle)
-		let view = nib.instantiateWithOwner(self, options: nil)[index] as UIView
+		let view = nib.instantiateWithOwner(self, options: nil)[index] as! UIView
 		return view
 	}
 }
@@ -117,7 +121,7 @@ class NibView: UIView, _NibView {
 	var nibIndex: Int { return 0 }
 	var view: UIView!
 	
-	convenience override init() {
+	convenience init() {
 		self.init(frame: .zeroRect)
 	}
 	
@@ -128,8 +132,8 @@ class NibView: UIView, _NibView {
 	
 	func setup() {
 		view = loadFromNib(nibName, index: nibIndex)
-		view.frame = self.bounds
-		view.autoresizingMask = UIViewAutoresizing.FlexibleHeight | UIViewAutoresizing.FlexibleWidth
+		view.frame = bounds
+		view.autoresizingMask = .FlexibleHeight | .FlexibleWidth
 		addSubview(view)
 	}
 	
@@ -141,7 +145,11 @@ class NibView: UIView, _NibView {
 
 extension UIImage {
 	func blend(color: UIColor) -> UIImage? {
-		let cgImage = self.CGImage.blend(kCGBlendModeDestinationIn, color: color.CGColor)
+		return blend(kCGBlendModeDestinationIn, color: color)
+	}
+	
+	func blend(mode: CGBlendMode, color: UIColor, alpha: CGFloat = 1) -> UIImage? {
+		let cgImage = CGImage.blend(mode, color: color.CGColor, alpha: alpha)
 		let image = UIImage(CGImage: cgImage, scale: scale, orientation: imageOrientation)
 		return image
 	}
@@ -188,7 +196,7 @@ extension UIViewController {
 		return nil
 	}
 	
-	func scrollView() -> UIScrollView? {
+	func scrollingView() -> UIScrollView? {
 		return nil
 	}
 	
@@ -197,28 +205,28 @@ extension UIViewController {
 	}
 	
 	func keyboardHeightDidUpdate(height: CGFloat) {
-		keyboardConstraint()?.constant = height
 	}
 	
 	func keyboardDidShow(notif: NSNotification) {
 		if let info = notif.userInfo {
-			var kbRect = (info["UIKeyboardBoundsUserInfoKey"] as NSValue).CGRectValue()
+			var kbRect = (info["UIKeyboardBoundsUserInfoKey"] as! NSValue).CGRectValue()
 			kbRect = self.view.convertRect(kbRect, fromView: nil)
 			if let constraint = keyboardConstraint() {
-				//				println(info)
-				keyboardHeightDidUpdate(kbRect.size.height)
-				UIView.animateWithDuration(info[UIKeyboardAnimationDurationUserInfoKey] as Double, delay: 0, options: UIViewAnimationOptions(info[UIKeyboardAnimationCurveUserInfoKey] as UInt), animations: {
+				let height = kbRect.size.height
+				constraint.constant = height
+				keyboardHeightDidUpdate(height)
+				UIView.animateWithDuration(info[UIKeyboardAnimationDurationUserInfoKey] as! Double, delay: 0, options: UIViewAnimationOptions(info[UIKeyboardAnimationCurveUserInfoKey] as! UInt), animations: {
 					self.view.layoutIfNeeded()
 					}, completion: nil)
 				return
 			}
 			let insets = UIEdgeInsetsMake(0, 0, kbRect.size.height, 0)
-			self.scrollView()?.contentInset = insets
-			self.scrollView()?.scrollIndicatorInsets = insets
+			self.scrollingView()?.contentInset = insets
+			self.scrollingView()?.scrollIndicatorInsets = insets
 			
 			if let field = self.activeField() {
 				let frame = field.convertRect(field.bounds, toView:self.view)
-				self.scrollView()?.scrollRectToVisible(frame, animated: true)
+				self.scrollingView()?.scrollRectToVisible(frame, animated: true)
 			}
 		}
 	}
@@ -227,14 +235,136 @@ extension UIViewController {
 		if let info = notif.userInfo {
 			let insets = UIEdgeInsetsZero
 			if let constraint = keyboardConstraint() {
+				constraint.constant = 0
 				keyboardHeightDidUpdate(0)
-				UIView.animateWithDuration(info[UIKeyboardAnimationDurationUserInfoKey] as Double) {
+				UIView.animateWithDuration(info[UIKeyboardAnimationDurationUserInfoKey] as! Double) {
 					self.view.layoutIfNeeded()
 				}
 				return
 			}
-			self.scrollView()?.contentInset = insets
-			self.scrollView()?.scrollIndicatorInsets = insets
+			self.scrollingView()?.contentInset = insets
+			self.scrollingView()?.scrollIndicatorInsets = insets
 		}
+	}
+}
+
+@IBDesignable
+class GradientView: UIView {
+	@IBInspectable var color1: UIColor = UIColor.whiteColor() { didSet { setNeedsDisplay() } }
+	@IBInspectable var color2: UIColor = UIColor.whiteColor() { didSet { setNeedsDisplay() } }
+	@IBInspectable var loc1: CGPoint = CGPointMake(0, 0) { didSet { setNeedsDisplay() } }
+	@IBInspectable var loc2: CGPoint = CGPointMake(1, 1) { didSet { setNeedsDisplay() } }
+	
+	override func drawRect(rect: CGRect) {
+		let context = UIGraphicsGetCurrentContext()
+		CGContextSaveGState(context)
+		let gradient = CGGradientCreateWithColors(CGColorSpaceCreateDeviceRGB(), [color1.CGColor, color2.CGColor], [0, 1])
+		CGContextDrawLinearGradient(context, gradient,
+			CGPointMake(rect.size.width * loc1.x, rect.size.height * loc1.y),
+			CGPointMake(rect.size.width * loc2.x, rect.size.height * loc2.y),
+			UInt32(kCGGradientDrawsBeforeStartLocation) | UInt32(kCGGradientDrawsAfterEndLocation))
+		CGContextRestoreGState(context)
+		super.drawRect(rect)
+	}
+}
+
+@IBDesignable
+class Gradient4View: UIView {
+	@IBInspectable var color1: UIColor = UIColor.whiteColor() { didSet { setNeedsDisplay() } }
+	@IBInspectable var color2: UIColor = UIColor.whiteColor() { didSet { setNeedsDisplay() } }
+	@IBInspectable var color3: UIColor = UIColor.whiteColor() { didSet { setNeedsDisplay() } }
+	@IBInspectable var color4: UIColor = UIColor.whiteColor() { didSet { setNeedsDisplay() } }
+	@IBInspectable var loc1: CGPoint = CGPointMake(0, 0.5) { didSet { setNeedsDisplay() } }
+	@IBInspectable var loc2: CGPoint = CGPointMake(0.22, 0.5) { didSet { setNeedsDisplay() } }
+	@IBInspectable var loc3: CGPoint = CGPointMake(0.41, 0.5) { didSet { setNeedsDisplay() } }
+	@IBInspectable var loc4: CGPoint = CGPointMake(1, 0.5) { didSet { setNeedsDisplay() } }
+	
+	override func drawRect(rect: CGRect) {
+		let context = UIGraphicsGetCurrentContext()
+		CGContextSaveGState(context)
+		let gradient = CGGradientCreateWithColors(CGColorSpaceCreateDeviceRGB(), [color1.CGColor, color2.CGColor, color3.CGColor, color4.CGColor], [loc1.x, loc2.x, loc3.x, loc4.x])
+		CGContextDrawLinearGradient(context, gradient,
+			CGPointMake(rect.size.width * loc1.x, rect.size.height * loc1.y),
+			CGPointMake(rect.size.width * loc4.x, rect.size.height * loc4.y),
+			UInt32(kCGGradientDrawsBeforeStartLocation) | UInt32(kCGGradientDrawsAfterEndLocation))
+		CGContextRestoreGState(context)
+		super.drawRect(rect)
+	}
+}
+
+struct Polar {
+	let r: CGFloat
+	var θ: CGFloat
+	
+	init(_ a: CGFloat, _ b: CGFloat) {
+		r = sqrt(pow(a, 2) + pow(b, 2))
+		θ = atan2(b, a)
+	}
+	
+	mutating func rotate(angle: CGFloat) {
+		θ -= angle
+	}
+	
+	var x: CGFloat {
+		return r * cos(θ)
+	}
+	
+	var y: CGFloat {
+		return r * sin(θ)
+	}
+}
+
+@IBDesignable
+class _Control: UIControl {
+	override func tintColorDidChange() {
+		setNeedsDisplay()
+	}
+	
+	@IBInspectable var disabledColor: UIColor = UIColor.grayColor()
+	
+	@IBInspectable var desiredWidth: CGFloat = 0 {
+		didSet { setNeedsDisplay() }
+	}
+	
+	@IBInspectable var angle: CGFloat = 0 {
+		didSet { setNeedsDisplay() }
+	}
+	
+	required override init(frame: CGRect) {
+		super.init(frame: frame)
+		if iOS8Less {
+			contentMode = .Redraw
+		}
+		opaque = false
+	}
+	
+	required init(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		if iOS8Less {
+			contentMode = .Redraw
+		}
+		opaque = false
+	}
+	
+	func centerScale(originalSize: CGSize, closure: (scale: CGFloat) -> ()) {
+		var width = desiredWidth
+		let rect = self.bounds
+		if width == 0 {
+			width = rect.size.width
+		}
+		let scale = width / originalSize.width
+		let context = UIGraphicsGetCurrentContext()
+		CGContextSaveGState(context)
+		CGContextTranslateCTM(context, (rect.size.width - width) / 2, (rect.size.height - width * originalSize.height / originalSize.width) / 2)
+		CGContextScaleCTM(context, scale, scale)
+		if enabled {
+			tintColor.setFill()
+			tintColor.setStroke()
+		} else {
+			disabledColor.setFill()
+			disabledColor.setStroke()
+		}
+		closure(scale: scale)
+		CGContextRestoreGState(context)
 	}
 }
