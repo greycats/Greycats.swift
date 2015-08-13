cd72b63fe5ad725021cbdc3d0bc2d4abd341ffd6
Rex Sheng <shengning@gmail.com>
2015-08-13 12:27:21 +0800

    update ReactControls functions. add UIControl? + UIControl? shortcut

--- Greycats/ReactControls.swift
+++ Greycats/ReactControls.swift
@@ -15,23 +15,34 @@ public class ReactControls: NSObject {
 		super.init()
 		self.a = a
 		self.b = b
+		a.addTarget(self, action: "sendBTouchUpInside", forControlEvents: .TouchUpInside)
+		a.addObserver(self, forKeyPath: "highlighted", options: .New, context: nil)
 		b.addObserver(self, forKeyPath: "highlighted", options: .New, context: nil)
 	}
 	
+	func sendBTouchUpInside() {
+		b.sendActionsForControlEvents(.TouchUpInside)
+	}
+	
 	deinit {
+		a.removeObserver(self, forKeyPath: "highlighted")
 		b.removeObserver(self, forKeyPath: "highlighted")
 	}
 	
 	override public func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
 		if keyPath == "highlighted" {
-			if b.highlighted {
-				UIView.animateWithDuration(0.02) {
-					self.a.alpha = 0.2
-				}
+			if object as? UIControl == a {
+				b.highlighted = a.highlighted
 			} else {
-				UIView.animateWithDuration(0.02, delay: 0.05, options: nil, animations: {
-					self.a.alpha = 1
-					}, completion: nil)
+				if b.highlighted {
+					UIView.animateWithDuration(0.02) {
+						self.a.alpha = 0.2
+					}
+				} else {
+					UIView.animateWithDuration(0.02, delay: 0.04, options: nil, animations: {
+						self.a.alpha = 1
+						}, completion: nil)
+				}
 			}
 		} else {
 			super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
@@ -39,3 +50,9 @@ public class ReactControls: NSObject {
 	}
 }
 
+public func +(lhs: UIControl?, rhs: UIControl?) -> AnyObject? {
+	if let a = lhs, let b = rhs {
+		return ReactControls(a: a, b: b)
+	}
+	return nil
+}
