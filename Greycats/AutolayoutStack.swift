62043a4af04c900ccaf43a0cda9c2a666eb1aba8
Rex Sheng <shengning@gmail.com>
2015-08-19 19:41:13 +0800

    update inject eject function

--- Greycats/AutolayoutStack.swift
+++ Greycats/AutolayoutStack.swift
@@ -13,6 +13,22 @@ public func _id<T: AnyObject>(object: T) -> String {
 	return "<\(_stdlib_getDemangledTypeName(object)): 0x\(String(ObjectIdentifier(object).uintValue, radix: 16))>"
 }
 
+func edge0(axis: UILayoutConstraintAxis) -> NSLayoutAttribute {
+	return axis == .Vertical ? .Top : .Leading
+}
+func edge1(axis: UILayoutConstraintAxis) -> NSLayoutAttribute {
+	return axis == .Vertical ? .Bottom : .Trailing
+}
+func perpendicularEdge0(axis: UILayoutConstraintAxis) -> NSLayoutAttribute {
+	return axis == .Horizontal ? .Top : .Leading
+}
+func perpendicularEdge1(axis: UILayoutConstraintAxis) -> NSLayoutAttribute {
+	return axis == .Horizontal ? .Bottom : .Trailing
+}
+func perpendicularDimension(axis: UILayoutConstraintAxis) -> NSLayoutAttribute {
+	return axis == .Horizontal ? .Height : .Width
+}
+
 extension UIView {
 	public func horizontalStack(views: [UIView], marginX: CGFloat = 0, equalWidth: Bool = false) -> [NSLayoutConstraint] {
 		for v in subviews {
@@ -55,15 +71,14 @@ extension UIView {
 		}
 		var previous: UIView? = nil
 		for view in views {
-			insertViewVertically(view, after: previous, marginX: marginX)
+			injectView(view, axis: .Vertical, after: previous, marginX: marginX)
 			previous = view
 		}
 	}
 	
 	func _previousView(view: UIView, axis: UILayoutConstraintAxis) -> NSLayoutConstraint? {
 		let gaps = constraints() as! [NSLayoutConstraint]
-		println(gaps)
-		let attr: NSLayoutAttribute = axis == .Vertical ? .Top : .Leading
+		let attr = edge0(axis)
 		for gap in gaps {
 			if gap.firstAttribute == attr && gap.firstItem as? UIView == view {
 				return gap
@@ -74,7 +89,7 @@ extension UIView {
 	
 	func _firstView(axis: UILayoutConstraintAxis) -> NSLayoutConstraint? {
 		let gaps = constraints() as! [NSLayoutConstraint]
-		let attr: NSLayoutAttribute = axis == .Vertical ? .Top : .Leading
+		let attr = edge0(axis)
 		for gap in gaps {
 			if gap.secondAttribute == attr && gap.secondItem as? UIView == self {
 				return gap
@@ -85,7 +100,7 @@ extension UIView {
 	
 	func _lastView(axis: UILayoutConstraintAxis) -> NSLayoutConstraint? {
 		let gaps = constraints() as! [NSLayoutConstraint]
-		let attr: NSLayoutAttribute = axis == .Vertical ? .Bottom : .Trailing
+		let attr = edge1(axis)
 		for gap in gaps {
 			if gap.firstAttribute == attr && gap.firstItem as? UIView == self {
 				return gap
@@ -96,7 +111,7 @@ extension UIView {
 	
 	func _nextView(view: UIView, axis: UILayoutConstraintAxis) -> NSLayoutConstraint? {
 		let gaps = constraints() as! [NSLayoutConstraint]
-		let attr: NSLayoutAttribute = axis == .Vertical ? .Bottom : .Trailing
+		let attr = edge1(axis)
 		for gap in gaps {
 			if gap.secondAttribute == attr && gap.secondItem as? UIView == view {
 				return gap
@@ -105,33 +120,73 @@ extension UIView {
 		return nil
 	}
 	
-	func insertViewVertically(view: UIView, after previous: UIView?, marginX: CGFloat = 0) {
+	public func ejectView(view: UIView, axis: UILayoutConstraintAxis) {
+		if let prev = _previousView(view, axis: axis),
+			let next = _nextView(view, axis: axis) {
+				let newConstraint = NSLayoutConstraint(item: next.firstItem, attribute: next.firstAttribute, relatedBy: .Equal, toItem: prev.secondItem, attribute: prev.secondAttribute, multiplier: 1, constant: view.bounds.height)
+				self.addConstraint(newConstraint)
+				self.layoutIfNeeded()
+				UIView.animateWithDuration(0.25, animations: {
+					view.alpha = 0
+					}) { _ in
+						self.removeConstraint(prev)
+						self.removeConstraint(next)
+						view.removeFromSuperview()
+				}
+				
+				UIView.animateWithDuration(0.15, delay: 0.2, options: .CurveEaseIn, animations: {
+					newConstraint.constant = 0
+					self.layoutIfNeeded()
+					}, completion: nil)
+		}
+	}
+	
+	public func injectView(view: UIView, axis: UILayoutConstraintAxis, after previous: UIView?, marginX: CGFloat = 0, animated: Bool = false) {
 		view.setTranslatesAutoresizingMaskIntoConstraints(false)
 		addSubview(view)
-		addConstraint(NSLayoutConstraint(item: view, attribute: .Leading, relatedBy: .Equal, toItem: self, attribute: .Leading, multiplier: 1, constant: marginX))
-		addConstraint(NSLayoutConstraint(item: view, attribute: .Trailing, relatedBy: .Equal, toItem: self, attribute: .Trailing, multiplier: 1, constant: -marginX))
-		addConstraint(NSLayoutConstraint(item: view, attribute: .Width, relatedBy: .Equal, toItem: self, attribute: .Width, multiplier: 1, constant: -2 * marginX))
+		let _edge0 = edge0(axis)
+		let _edge1 = edge1(axis)
+		let pedge0 = perpendicularEdge0(axis)
+		let pedge1 = perpendicularEdge1(axis)
+		let attr = perpendicularDimension(axis)
+		addConstraint(NSLayoutConstraint(item: view, attribute: pedge0, relatedBy: .Equal, toItem: self, attribute: pedge0, multiplier: 1, constant: marginX))
+		addConstraint(NSLayoutConstraint(item: view, attribute: pedge1, relatedBy: .Equal, toItem: self, attribute: pedge1, multiplier: 1, constant: -marginX))
+		addConstraint(NSLayoutConstraint(item: view, attribute: attr, relatedBy: .Equal, toItem: self, attribute: attr, multiplier: 1, constant: -2 * marginX))
 		
-		let gaps: [NSLayoutConstraint] = constraintsAffectingLayoutForAxis(.Vertical) as! [NSLayoutConstraint]
+		let edge0Constraint: NSLayoutConstraint
 		if let previous = previous {
 			// let us found original next view, and link it to this view
-			if let c = _nextView(previous, axis: .Vertical) {
+			if let c = _nextView(previous, axis: axis) {
 				removeConstraint(c)
-				addConstraint(NSLayoutConstraint(item: c.firstItem, attribute: c.firstAttribute, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1, constant: c.constant))
-			}
-			let top = NSLayoutConstraint(item: view, attribute: .Top, relatedBy: .Equal, toItem: previous, attribute: .Bottom, multiplier: 1, constant: 0)
-			addConstraint(top)
-		} else {
-			// view is gonna be first, find current first and unlink it
-			if let c = _firstView(.Vertical) {
-				addConstraint(NSLayoutConstraint(item: c.firstItem, attribute: .Top, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1, constant: 0))
-				removeConstraint(c)
-			} else {
-				let bottom = NSLayoutConstraint(item: self, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1, constant: 2)
+				let bottom = NSLayoutConstraint(item: c.firstItem, attribute: c.firstAttribute, relatedBy: .Equal, toItem: view, attribute: _edge1, multiplier: 1, constant: c.constant)
 				addConstraint(bottom)
 			}
-			let top = NSLayoutConstraint(item: view, attribute: .Top, relatedBy: .Equal, toItem: self, attribute: .Top, multiplier: 1, constant: 0)
-			addConstraint(top)
+			edge0Constraint = NSLayoutConstraint(item: view, attribute: _edge0, relatedBy: .Equal, toItem: previous, attribute: _edge1, multiplier: 1, constant: 0)
+		} else {
+			// view is gonna be first, find current first and unlink it
+			if let c = _firstView(axis) {
+				addConstraint(NSLayoutConstraint(item: c.firstItem, attribute: _edge0, relatedBy: .Equal, toItem: view, attribute: _edge1, multiplier: 1, constant: 0))
+				removeConstraint(c)
+			} else {
+				let bottom = NSLayoutConstraint(item: self, attribute: _edge1, relatedBy: .Equal, toItem: view, attribute: _edge1, multiplier: 1, constant: 2)
+				addConstraint(bottom)
+			}
+			edge0Constraint = NSLayoutConstraint(item: view, attribute: _edge0, relatedBy: .Equal, toItem: self, attribute: _edge0, multiplier: 1, constant: 0)
+		}
+		addConstraint(edge0Constraint)
+		if animated {
+			let size = view.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize)
+			edge0Constraint.constant = axis == .Vertical ? -size.height : -size.width
+			layoutIfNeeded()
+			view.alpha = 0
+			UIView.animateWithDuration(0.25) {
+				edge0Constraint.constant = 0
+				self.layoutIfNeeded()
+			}
+			
+			UIView.animateWithDuration(0.15, delay: 0.2, options: .CurveEaseIn, animations: {
+				view.alpha = 1
+				}, completion: nil)
 		}
 	}
 }
