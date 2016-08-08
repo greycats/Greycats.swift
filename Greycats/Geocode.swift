9f2652db2952edc24f599fe80d9f501661a02c24
Rex Sheng <shengning@gmail.com>
2016-08-08 14:04:01 +0800

    fix Geocode

--- Greycats/Geocode.swift
+++ Greycats/Geocode.swift
@@ -9,66 +9,82 @@
 import CoreLocation
 
 public enum Geocode {
-	class GeocodeContainer: NSObject, CLLocationManagerDelegate {
-		var locationManager: CLLocationManager!
+    class GeocodeContainer: NSObject, CLLocationManagerDelegate {
+        var locationManager: CLLocationManager!
 
-		var callback: (CLLocation? -> Void)?
+        var callback: (CLLocation? -> Void)?
 
-		func current(accuracy: CLLocationAccuracy, callback: CLLocation? -> Void) {
-			locationManager?.stopUpdatingLocation()
-			self.callback = callback
-			if locationManager == nil {
-				locationManager = CLLocationManager()
-				locationManager.delegate = self
-				locationManager.desiredAccuracy = accuracy
-//				if #available(iOS 9.0, *) {
-//					locationManager.requestLocation()
-//				} else {
-					locationManager.requestWhenInUseAuthorization()
-					locationManager.startUpdatingLocation()
-//				}
-			}
-		}
+        func current(accuracy: CLLocationAccuracy, callback: CLLocation? -> Void) {
+            locationManager?.stopUpdatingLocation()
+            self.callback = callback
+            if locationManager == nil {
+                locationManager = CLLocationManager()
+                locationManager.delegate = self
+                switch CLLocationManager.authorizationStatus() {
+                case .AuthorizedAlways, .AuthorizedWhenInUse:
+                    requestLocation()
+                case .NotDetermined:
+                    locationManager.requestWhenInUseAuthorization()
+                default:
+                    callback(nil)
+                    self.callback = nil
+                }
+            }
+        }
 
-		func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-			callback?(locations.last)
-			callback = nil
-//			if #available(iOS 9.0, *) {
-//			} else {
-				manager.stopUpdatingLocation()
-//			}
-		}
+        func requestLocation() {
+            if #available(iOS 9.0, *) {
+                locationManager.requestLocation()
+            } else {
+                locationManager.startUpdatingLocation()
+            }
+        }
 
-		func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
-			callback?(manager.location)
-			callback = nil
-			print("location failed with error \(error.localizedDescription)")
-//			if #available(iOS 9.0, *) {
-//			} else {
-				manager.stopUpdatingLocation()
-//			}
-		}
+        func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            callback?(locations.last)
+            callback = nil
+            if #available(iOS 9.0, *) {
+            } else {
+                manager.stopUpdatingLocation()
+            }
+        }
 
-		deinit {
-			print("deinit GeocodeContainer")
-		}
-	}
+        func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+            switch status {
+            case .AuthorizedAlways, .AuthorizedWhenInUse:
+                requestLocation()
+            default:
+                locationManager = nil
+                callback?(nil)
+                callback = nil
+            }
+        }
 
-	private static var geoContainer = GeocodeContainer()
-	case Location(CLLocation)
-	case Current(CLLocationAccuracy)
+        func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+            callback?(manager.location)
+            callback = nil
+            print("location failed with error \(error.localizedDescription)")
+            if #available(iOS 9.0, *) {
+            } else {
+                manager.stopUpdatingLocation()
+            }
+        }
 
-	public func getLocation(closure: (CLLocation?) -> ()) {
-		switch self {
-		case .Location(let location):
-			closure(location)
-		case .Current(let accuracy):
-			switch CLLocationManager.authorizationStatus() {
-			case .AuthorizedAlways, .AuthorizedWhenInUse:
-				Geocode.geoContainer.current(accuracy, callback: closure)
-			default:
-				closure(nil)
-			}
-		}
-	}
+        deinit {
+            print("deinit GeocodeContainer")
+        }
+    }
+
+    private static var geoContainer = GeocodeContainer()
+    case Location(CLLocation)
+    case Current(CLLocationAccuracy)
+
+    public func getLocation(closure: (CLLocation?) -> ()) {
+        switch self {
+        case .Location(let location):
+            closure(location)
+        case .Current(let accuracy):
+            Geocode.geoContainer.current(accuracy, callback: closure)
+        }
+    }
 }
