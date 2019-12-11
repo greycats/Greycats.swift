03252ee0763b4f82d26da3703b24422debcca1a4
Victor Zhu <ghoyelo@gmail.com>
2019-12-11 16:08:07 +0800

    Update nib subview with autolayout

--- Greycats/Layout/NibView.swift
+++ Greycats/Layout/NibView.swift
@@ -69,7 +69,11 @@ extension NibViewProtocol where Self: UIView {
         view.backgroundColor = nil
         view.frame = bounds
         insertSubview(view, at: 0)
-        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.translatesAutoresizingMaskIntoConstraints = false
+        addConstraint(NSLayoutConstraint(item: view, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: view, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 0))
+        addConstraint(NSLayoutConstraint(item: view, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1, constant: 0))
         objc_setAssociatedObject(self, &viewKey, view, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         return view
     }
